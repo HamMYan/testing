975d3c697b67ee4e7d21747d9c34fa8b11eeb02a
HamletMYan <hammkrtchyan7@gmail.com>
2024-07-24 19:21:16 +0400

    first commit

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id:number;
+    
+    @Column()
+    name:string
+    
+    @Column()
+    surname:string
+    
+    @Column()
+    age:number
+    
+    @Column()
+    email:string
+    
+    @Column()
+    salary:number
+}
